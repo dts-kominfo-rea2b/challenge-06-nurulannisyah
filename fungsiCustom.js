e3791d0f47012300529e5c4b2acbf2bf550cad4f
nurulannisyah <ayu23nurul@gmail.com>
2022-06-08 15:26:02 +0700

    Tugas Callback Part 2

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,6 @@
 // TODO: import module bila dibutuhkan di sini
+const { throws } = require('assert');
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +20,35 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  
+  arrHasil = [];
+
+  fs.readFile(file1, {encoding: "utf-8"}, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    } 
+    
+    const jsonVar1 = JSON.parse(data);
+    const txt = jsonVar1.message.split(" ");
+    arrHasil.push(txt[1]);
+    
+    fs.readFile(file2, {encoding: "utf-8"}, (err, data) => {
+      if (err) { return fnCallback(err, null); } 
+      
+      const jsonVar2 = JSON.parse(data);
+      arrHasil.push(jsonVar2[0].message.split("halo ").join(""));
+
+      fs.readFile(file3, {encoding: "utf-8"}, (err, data) => {
+        if (err) { return fnCallback(err, null) }
+        const jsonVar3 = JSON.parse(data);
+        arrHasil.push(jsonVar3[0]['data'].message.split("halo ").join(""));
+        fnCallback(null, arrHasil);
+      });
+
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
